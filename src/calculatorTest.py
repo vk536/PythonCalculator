8f5da53a18eda253ba5833487f757eb01335a8b7
vivek <vk536@njit.edu>
2020-10-30 19:42:48 -0400

    Feature: Adding testcase, code for subtraction and the subtraction CSV file.

--- src/calculatorTest.py
+++ src/calculatorTest.py
@@ -15,6 +15,13 @@ class MyTestCase(unittest.TestCase):
             self.assertEqual(self.calculator.add(row['Value 1'], row['Value 2']), result)
             self.assertEqual(self.calculator.result, result)
 
+    def test_subtraction_calculator(self):
+        test_data = CsvReader("src/CsvTestFiles/Unit Test Subtraction.csv").data
+        for row in test_data:
+            result = int(row['Result'])
+            self.assertEqual(self.calculator.sub(row['Value 1'], row['Value 2']), result)
+            self.assertEqual(self.calculator.result, result)
+
 
 if __name__ == '__main__':
     unittest.main()
